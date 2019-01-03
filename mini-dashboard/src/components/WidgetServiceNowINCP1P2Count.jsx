919174b19724179c0626d029272b8280f5b2fd43
cartacode <cartacode@outlook.com>
2019-01-03 10:11:42 -0500

    streamlined the api proxy call

--- mini-dashboard/src/components/WidgetServiceNowINCP1P2Count.jsx
+++ mini-dashboard/src/components/WidgetServiceNowINCP1P2Count.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import DashboardCard from "./DashboardCard";
-import servicenow from "../api/servicenow";
+import apiProxy from "../api/apiProxy";
 
 // Create a widget class ----
 class WidgetServiceNowP1P2Count extends React.Component {
@@ -11,10 +11,9 @@ class WidgetServiceNowP1P2Count extends React.Component {
     }
 
     componentDidMount = async () => {
-        const response = await servicenow.get(
-            "http://192.168.1.177:8000/servicenow-proxy/jnjsandbox.service-now.com/api/now/stats/incident?sysparm_query=stateIN100%2C2%5EpriorityIN1%2C2&sysparm_count=true",
-            {}
-        );
+        const response = await apiProxy.get("/sn/jnjsandbox.service-now.com/api/now/stats/incident", {
+            params: { sysparm_query: "stateIN100,2^priorityIN1,2", sysparm_count: "true" }
+        });
         console.log(response);
         this.setState({ count: response.data.result.stats.count });
     };
@@ -22,7 +21,8 @@ class WidgetServiceNowP1P2Count extends React.Component {
     renderCardBody() {
         return (
             <div>
-                <h1>Count: {this.state.count}</h1>
+                <div className="single-num-title">P1/P2 Incidents</div>
+                <div className="single-num-value">{this.state.count}</div>
             </div>
         );
     }
