99f2c0df1d9e68e45cd35ae62945b66b3b9f1178
cartacode <cartacode@outlook.com>
2019-01-11 20:59:33 -0500

    add switch for environment

--- src/api/apiProxy.jsx
+++ src/api/apiProxy.jsx
@@ -1,7 +1,14 @@
 import axios from "axios";
 
+let baseURL = "http://no_env_defined:8000";
+
+if (process.env.NODE_ENV === "production") {
+    baseURL = "http://10.37.63.17:1521";
+} else if (process.env.NODE_ENV === "development") {
+    baseURL = "http://localhost:8000";
+}
 export default axios.create({
-    baseURL: "http://localhost:8000",
+    baseURL: baseURL,
     // baseURL: "http://10.37.63.17:1521",
     headers: {
         Authorization: "Basic YmlsbHlib2I6OThUZUAmNyVmYnhjeSRNSw=="
