3a90475e700a419130a00c2e9ea82b5b253bcc2f
cartacode <cartacode@outlook.com>
2019-01-11 20:39:35 -0500

    Adding express config

--- src/api/apiProxy.jsx
+++ src/api/apiProxy.jsx
@@ -2,6 +2,7 @@ import axios from "axios";
 
 export default axios.create({
     baseURL: "http://localhost:8000",
+    // baseURL: "http://10.37.63.17:1521",
     headers: {
         Authorization: "Basic YmlsbHlib2I6OThUZUAmNyVmYnhjeSRNSw=="
     }
