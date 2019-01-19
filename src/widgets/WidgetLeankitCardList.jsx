7835d6c4ebb6b17c816d5494e9ea53e2594b1aa1
cartacode <cartacode@outlook.com>
2019-01-18 21:04:07 -0500

    working...

--- src/widgets/WidgetLeankitCardList.jsx
+++ src/widgets/WidgetLeankitCardList.jsx
@@ -1,8 +1,8 @@
 import React from "react";
 import DashboardDataCard from "../components/DashboardDataCard";
 import apiProxy from "../api/apiProxy";
-import PropTypes from "prop-types";
 import { determineOSFromUserAuth } from "../utilities/determineOSFromUserAuth";
+var classNames = require("classnames");
 
 // Create a class component
 class WidgetLeankitCardList extends React.Component {
@@ -12,48 +12,11 @@ class WidgetLeankitCardList extends React.Component {
     }
 
     componentDidMount = async () => {
-        let startTime = new Date();
-
-        // Load the data from the API (notice we're using the await keyword from the async framework)
-        let response_cards = await apiProxy.get(
-            `/leankit-api/${this.state.instance}/io/card?board=412731036&limit=60&lane_class_types=active`
-        );
-
-        // Setup dummy entries for custom fields
-        response_cards.data.cards.map(card => {
-            card.u_lanes = [{}, {}, {}];
-        });
-
-        let response_lanes = await apiProxy.get(`/leankit-api/${this.state.instance}/io/board/412731036`);
-        console.log(response_cards.data.cards);
-        console.log(response_lanes.data.lanes);
-
-        // Save into our component state
-        this.setState({ leankit_cards: response_cards.data.cards });
-        this.setState({ leankit_lanes: response_lanes.data.lanes });
-
-        // Create an object of all lanes, indexed by lane Id
-        let lanesById = {};
-        response_lanes.data.lanes.map(lane => {
-            lanesById[lane.id] = lane;
-        });
-
-        // Card info
-        response_cards.data.cards.map(card => {
-            // Push the card's own lane
-            card.u_lanes[0] = lanesById[card.lane.id];
-            // Push the first parent
-            card.u_lanes[1] = lanesById[lanesById[card.u_lanes[0].id].parentLaneId];
-            // push the second parent
-            card.u_lanes[2] = lanesById[card.u_lanes[1].parentLaneId];
-        });
-        console.log(response_cards.data.cards);
-
-        let endTime = new Date();
-        console.log("Time: ", Math.round(endTime - startTime));
-
-        this.setState({ leankit_cards: response_cards.data.cards });
+        let leankit_cards = await this.getLeankitCards();
+        this.setState({ leankit_cards: leankit_cards });
     };
+
+    renderTableRow() {}
 
     renderTable() {
         if (this.state.leankit_cards === []) {
@@ -70,7 +33,9 @@ class WidgetLeankitCardList extends React.Component {
                                     {/* <td>{card["updatedOn"]}%</td> */}
                                     <td>{(card.u_lanes[0] && card.u_lanes[0].name) || "No parent"}</td>
                                     <td>{(card.u_lanes[1] && card.u_lanes[1].name) || "No parent"}</td>
-                                    <td>{(card.u_lanes[2] && card.u_lanes[2].name) || "No parent"}</td>
+                                    <td className={classNames({ tdRed: true })}>
+                                        {(card.u_lanes[2] && card.u_lanes[2].name) || "No parent"}
+                                    </td>
                                     {/* <td>{("u_lanes" in card && card.u_lanes[0].name) || "No parent"}</td> */}
                                     {/* <td>{card["count"]}</td> */}
                                 </tr>
@@ -105,43 +70,52 @@ class WidgetLeankitCardList extends React.Component {
     }
 
     // ########################################################################################
-    // ########################################################################################
-    // ########################################################################################
 
-    createOSCounts(user_agent_table) {
-        // Print out all user_agent strings for trouble-shooting
-        console.log("All user_agent strings:");
-        user_agent_table.forEach(row => {
-            // console.log(`${row.count}: ${row.user_agent}`);
+    getLeankitCards = async function() {
+        let startTime = new Date();
+
+        // Load the data from the API (notice we're using the await keyword from the async framework)
+        let response_cards_promise = apiProxy.get(
+            `/leankit-api/${this.state.instance}/io/card?board=412731036&limit=60&lane_class_types=active`
+        );
+        let response_lanes_promise = apiProxy.get(`/leankit-api/${this.state.instance}/io/board/412731036`);
+        // Wait for both promises to finish
+        let [response_cards, response_lanes] = [await response_cards_promise, await response_lanes_promise];
+
+        // Setup dummy entries for custom fields
+        response_cards.data.cards.map(card => {
+            card.u_lanes = [{}, {}, {}];
         });
 
-        let OSCountObj = {};
+        console.log(response_cards.data.cards);
+        console.log(response_lanes.data.lanes);
 
-        user_agent_table.forEach(element => {
-            // this function is an external module which we import, see top of file
-            let osName = determineOSFromUserAuth(element["user_agent"]);
+        // Save into our component state
+        this.setState({ leankit_cards: response_cards.data.cards });
+        this.setState({ leankit_lanes: response_lanes.data.lanes });
 
-            // Accumulate a count of each OS
-            OSCountObj[osName] = (osName in OSCountObj ? OSCountObj[osName] : 0) + parseInt(element["count"]);
+        // Create an object of all lanes, indexed by lane Id
+        let lanesById = {};
+        response_lanes.data.lanes.map(lane => {
+            lanesById[lane.id] = lane;
         });
 
-        // Get total of all OS counts
-        let OSTotal = Object.values(OSCountObj).reduce((total, num) => {
-            return total + num;
+        // Insert lane info into each card (u_ because its a custom field)
+        response_cards.data.cards.map(card => {
+            // Push the card's own lane
+            card.u_lanes[0] = lanesById[card.lane.id];
+            // Push the first parent
+            card.u_lanes[1] = lanesById[lanesById[card.u_lanes[0].id].parentLaneId];
+            // push the second parent
+            card.u_lanes[2] = lanesById[card.u_lanes[1].parentLaneId];
         });
+        console.log(response_cards.data.cards);
 
-        // Construct OS object with percentages
-        for (const key of Object.keys(OSCountObj)) {
-            OSCountObj[key] = {
-                count: OSCountObj[key],
-                pct: (OSCountObj[key] / OSTotal) * 100,
-                name: key
-            };
-        }
+        let endTime = new Date();
+        console.log("Time: ", Math.round(endTime - startTime));
 
-        return OSCountObj;
-    }
-
+        return response_cards.data.cards;
+    };
     // end of class
 }
 
